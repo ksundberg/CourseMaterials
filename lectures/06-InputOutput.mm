f95e62e1e80c7c04bc62cb239e53a1baa051a63b
Kenneth Sundberg <Kenneth.Sundberg@sdl.usu.edu>
2015-06-25 12:19:30 -0600

    Add new lecture for I/O update more annotations

---
+++ lectures/06-InputOutput.mm
@@ -0,0 +1,90 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="06 - Input and Output" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1435255531607"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<node TEXT="temp" POSITION="right" ID="ID_255570621" CREATED="1435255533088" MODIFIED="1435255606274">
+<edge COLOR="#ff0000"/>
+<node TEXT="textbook sections" ID="ID_1137739982" CREATED="1435255606279" MODIFIED="1435255610896">
+<node TEXT="string class" ID="ID_1038725498" CREATED="1435255610899" MODIFIED="1435255631437">
+<attribute NAME="textbook" VALUE="2-10"/>
+</node>
+<node TEXT="formatting output" ID="ID_1673670152" CREATED="1435255652880" MODIFIED="1435255671535">
+<attribute NAME="textbook" VALUE="3-07"/>
+</node>
+<node TEXT="working with characters and strings" ID="ID_1392216909" CREATED="1435255681064" MODIFIED="1435255696457">
+<attribute NAME="textbook" VALUE="3-08"/>
+</node>
+<node TEXT="c-strings" ID="ID_1304476674" CREATED="1435255703384" MODIFIED="1435255718881">
+<attribute NAME="textbook" VALUE="3-09"/>
+</node>
+<node TEXT="Menu driven programs" ID="ID_766762115" CREATED="1435255973944" MODIFIED="1435255991719">
+<attribute NAME="textbook" VALUE="4-05"/>
+</node>
+<node TEXT="Validating input" ID="ID_470859547" CREATED="1435255993744" MODIFIED="1435256018439">
+<attribute NAME="textbook" VALUE="4-08"/>
+</node>
+<node TEXT="More about char and string" ID="ID_1545198863" CREATED="1435256031506" MODIFIED="1435256044900">
+<attribute NAME="textbook" VALUE="4-10"/>
+</node>
+<node TEXT="While loops for input validation" ID="ID_1238120148" CREATED="1435256073578" MODIFIED="1435256087695">
+<attribute NAME="textbook" VALUE="5-02"/>
+</node>
+<node TEXT="Using files" ID="ID_1428076075" CREATED="1435256138761" MODIFIED="1435256173231">
+<attribute NAME="textbook" VALUE="5-12"/>
+</node>
+<node TEXT="Using Functions in menu driven program" ID="ID_1256132293" CREATED="1435256197552" MODIFIED="1435256217878">
+<attribute NAME="textbook" VALUE="6-09"/>
+</node>
+</node>
+</node>
+</node>
+</map>
