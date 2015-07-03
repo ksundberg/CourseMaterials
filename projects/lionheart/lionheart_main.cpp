e7f960be5ebbbb93c92a3753533baa29ce352e82
Kenneth Sundberg <Kenneth.Sundberg@sdl.usu.edu>
2015-07-02 23:27:41 -0600

    Add other strategies

--- projects/lionheart/lionheart_main.cpp
+++ projects/lionheart/lionheart_main.cpp
@@ -6,15 +6,23 @@
 #include "Player/Goldfish.hpp"
 #include "Player/ChargingBadger.hpp"
 #include "Player/WildBoar.hpp"
+#include "Player/SnappingTurtle.hpp"
+#include "Player/CirclingShark.hpp"
+#include "Player/CrouchingTiger.hpp"
+#include "Player/HiddenDragon.hpp"
 
 int main(void)
 {
+  auto display = std::make_shared<lionheart::AnsiDisplay>();
   std::vector<std::shared_ptr<lionheart::Player>> players;
   players.push_back(std::make_shared<lionheart::ChargingBadger>());
   players.push_back(std::make_shared<lionheart::SittingDuck>());
   players.push_back(std::make_shared<lionheart::WildBoar>());
   players.push_back(std::make_shared<lionheart::Goldfish>());
-  auto display = std::make_shared<lionheart::AnsiDisplay>();
+  players.push_back(std::make_shared<lionheart::SnappingTurtle>());
+  players.push_back(std::make_shared<lionheart::CirclingShark>());
+  players.push_back(std::make_shared<lionheart::CrouchingTiger>());
+  players.push_back(std::make_shared<lionheart::HiddenDragon>());
   lionheart::SingleElimination t(players,display);
   t.run();
 }
