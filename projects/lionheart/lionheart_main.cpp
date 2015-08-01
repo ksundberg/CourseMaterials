d60c7fc800eae6eb2dbd2a5d19367bf47304ca1b
Kenneth Sundberg <kasundberg@gmail.com>
2015-07-31 18:49:59 -0600

    Configure new getPlayers based on CMakeLists to make adding new players easy

--- projects/lionheart/lionheart_main.cpp
+++ projects/lionheart/lionheart_main.cpp
@@ -2,27 +2,12 @@
 #include "Plan.hpp"
 #include "SingleElimination.hpp"
 #include "AnsiDisplay.hpp"
-#include "Player/SittingDuck.hpp"
-#include "Player/Goldfish.hpp"
-#include "Player/ChargingBadger.hpp"
-#include "Player/WildBoar.hpp"
-#include "Player/SnappingTurtle.hpp"
-#include "Player/CirclingShark.hpp"
-#include "Player/CrouchingTiger.hpp"
-#include "Player/HiddenDragon.hpp"
+#include "getPlayers.hpp"
 
 int main(void)
 {
   auto display = std::make_shared<lionheart::AnsiDisplay>();
-  std::vector<std::shared_ptr<lionheart::Player>> players;
-  players.push_back(std::make_shared<lionheart::ChargingBadger>());
-  players.push_back(std::make_shared<lionheart::SittingDuck>());
-  players.push_back(std::make_shared<lionheart::WildBoar>());
-  players.push_back(std::make_shared<lionheart::Goldfish>());
-  players.push_back(std::make_shared<lionheart::SnappingTurtle>());
-  players.push_back(std::make_shared<lionheart::CirclingShark>());
-  players.push_back(std::make_shared<lionheart::CrouchingTiger>());
-  players.push_back(std::make_shared<lionheart::HiddenDragon>());
+  auto players = lionheart::getPlayers();
   lionheart::SingleElimination t(players,display);
   t.run();
 }
