88c9d2cb2718ac7362674184be0ba1907e261ad0
Kenneth Sundberg <ksundberg@sdl.usu.edu>
2016-08-18 16:00:56 -0600

    Introduction lecture (not complete)

---
+++ cs3450/lectures/01-Introduction.mm
@@ -0,0 +1,210 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="01- Introduction" ID="ID_729166855" CREATED="1430766178119" MODIFIED="1471555533840">
+<attribute NAME="author" VALUE="Kenneth Sundberg"/>
+<attribute NAME="date" VALUE=""/>
+<attribute NAME="subtitle" VALUE="CS 3450"/>
+<hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Syllabus" POSITION="right" ID="ID_405778426" CREATED="1430766242153" MODIFIED="1432567614206">
+<node TEXT="Topic Overview" ID="ID_1478149906" CREATED="1430766266105" MODIFIED="1430766274717">
+<node TEXT="Course Description" ID="ID_1245100126" CREATED="1433013226707" MODIFIED="1433013240413">
+<node TEXT="Introduction to science of problem solving, programming, program development, algorithm analysis, and data structures. Students will learn to develop correct software in a current programming language environment." ID="ID_1608230200" CREATED="1433013260065" MODIFIED="1433013341110"/>
+</node>
+<node TEXT="Textbook" ID="ID_787170715" CREATED="1433014013454" MODIFIED="1433014016745">
+<node TEXT="Beginning Software Engineering" ID="ID_382147496" CREATED="1433014016765" MODIFIED="1471555570047"/>
+<node TEXT="ISBN: 978-1-118-96914-4" ID="ID_1932376874" CREATED="1433014065333" MODIFIED="1471555595529"/>
+</node>
+<node TEXT="Course Objectives" ID="ID_1253672214" CREATED="1433013240962" MODIFIED="1433013246132">
+<node TEXT="To develop mastery level of proficiency in: data types, conditional operations, looping structures, functions, classes, objects and arrays" ID="ID_1462950834" CREATED="1433013620304" MODIFIED="1433013660111"/>
+</node>
+<node TEXT="Student Objectives" ID="ID_215922869" CREATED="1433013661443" MODIFIED="1433013668735">
+<node TEXT="Develop an ability to design, implement, and evaluate a computer based system, process, component, or program to meet desired needs." ID="ID_1073513133" CREATED="1433013668747" MODIFIED="1433013705039"/>
+</node>
+</node>
+<node TEXT="Policies" ID="ID_1049242193" CREATED="1430766258561" MODIFIED="1430766265555">
+<node TEXT="Academic Honesty" ID="ID_1414885647" CREATED="1433013754576" MODIFIED="1433013775443">
+<node TEXT="It is your responsibility to pursue your education in an honest way" LOCALIZED_STYLE_REF="styles.topic" ID="ID_1395850290" CREATED="1433013775461" MODIFIED="1433013848269"/>
+<node TEXT="Plagiarism and cheating are not tolerated" ID="ID_1338850376" CREATED="1433013811174" MODIFIED="1433013839327"/>
+<node TEXT="This course involves group work be careful to contribute" ID="ID_805205164" CREATED="1471555619235" MODIFIED="1471555645594"/>
+</node>
+<node TEXT="Legalized Decency" ID="ID_1011156071" CREATED="1433013883972" MODIFIED="1433013909949">
+<node TEXT="ADA" ID="ID_1538023574" CREATED="1433013909960" MODIFIED="1433013923750"/>
+<node TEXT="Sexual Harassment is not okay" ID="ID_46842519" CREATED="1433013924377" MODIFIED="1433013956596"/>
+<node TEXT="Academic Freedom and the Faculty Code" ID="ID_546616331" CREATED="1433013975944" MODIFIED="1433013986979"/>
+</node>
+<node TEXT="Late Policy" ID="ID_306869006" CREATED="1433014313508" MODIFIED="1433014317564">
+<node TEXT="Don&apos;t be late" LOCALIZED_STYLE_REF="styles.topic" ID="ID_892500312" CREATED="1433014317573" MODIFIED="1433014343582"/>
+<node TEXT="Lose 10 points each day" ID="ID_1612600366" CREATED="1433014325572" MODIFIED="1433014361141"/>
+<node TEXT="No work accepted after 3 days" LOCALIZED_STYLE_REF="styles.topic" ID="ID_1644515651" CREATED="1433014361506" MODIFIED="1433014397132"/>
+<node TEXT="No make up work, extra credit or other silliness will be offered" ID="ID_1814432677" CREATED="1433014369265" MODIFIED="1433014389581"/>
+</node>
+</node>
+<node TEXT="Grading" ID="ID_565993697" CREATED="1430766279625" MODIFIED="1430766291435">
+<node TEXT="Grade Weights" ID="ID_505058420" CREATED="1433015005586" MODIFIED="1433015035668">
+<node TEXT="Assignments: 50" ID="ID_1423457656" CREATED="1433015035685" MODIFIED="1471555715770"/>
+<node TEXT="Peer Evaluations: 10" ID="ID_715090175" CREATED="1471555690851" MODIFIED="1471555700491"/>
+<node TEXT="Quizzes: 10" ID="ID_635252124" CREATED="1433015056818" MODIFIED="1433015060980"/>
+<node TEXT="Exams: 30" ID="ID_1643166868" CREATED="1433015061442" MODIFIED="1471555684711"/>
+</node>
+<node TEXT="Grade Breakdown" ID="ID_980054680" CREATED="1433014753805" MODIFIED="1433014922581">
+<node TEXT="94: A" ID="ID_865688387" CREATED="1433014805743" MODIFIED="1433014863347"/>
+<node TEXT="90: A-" ID="ID_1489290609" CREATED="1433014846009" MODIFIED="1433014859347"/>
+<node TEXT="87: B+" ID="ID_1990024942" CREATED="1433014864432" MODIFIED="1433014874347"/>
+<node TEXT="84: B" ID="ID_1949740908" CREATED="1433014875089" MODIFIED="1433014880276"/>
+<node TEXT="80: B-" ID="ID_348847779" CREATED="1433014880816" MODIFIED="1433014885827"/>
+<node TEXT="77: C+" ID="ID_1841544540" CREATED="1433014886504" MODIFIED="1433014891626"/>
+<node TEXT="74: C" ID="ID_476899600" CREATED="1433014892191" MODIFIED="1433014896091"/>
+<node TEXT="70: C-" ID="ID_188194848" CREATED="1433014896959" MODIFIED="1433014900297"/>
+<node TEXT="Others: F" ID="ID_1629673453" CREATED="1433014901279" MODIFIED="1433014904640"/>
+</node>
+<node TEXT="Curving" ID="ID_704391856" CREATED="1433014923383" MODIFIED="1433014925815">
+<node TEXT="I will never curve down" ID="ID_454219165" CREATED="1433014925827" MODIFIED="1433014937362"/>
+<node TEXT="At my discretion I may curve up at the end of the semester" ID="ID_1977281405" CREATED="1433014937806" MODIFIED="1433014962216"/>
+<node TEXT="Such curves are often small (1 or 2 points at most)" ID="ID_1291513933" CREATED="1433014962717" MODIFIED="1433014990627"/>
+</node>
+<node TEXT="Assignment Grading" ID="ID_1399304959" CREATED="1433015114432" MODIFIED="1433015120592">
+<node TEXT="Assignments have the most weight on purpose" ID="ID_1961528815" CREATED="1433015120600" MODIFIED="1433015135937"/>
+<node TEXT="You can fail every test and quiz and still pass if you do well on the assignments" LOCALIZED_STYLE_REF="styles.topic" ID="ID_969848273" CREATED="1433015136270" MODIFIED="1433015222419"/>
+<node TEXT="Assignments have clear objective grading criteria" ID="ID_850867986" CREATED="1433015193180" MODIFIED="1433015214410"/>
+<node TEXT="I expect most students to get nearly all of the assignment points" ID="ID_933019687" CREATED="1433015156374" MODIFIED="1433015192583"/>
+</node>
+</node>
+</node>
+<node TEXT="Introduction" POSITION="right" ID="ID_1555719218" CREATED="1430766293248" MODIFIED="1430766339763">
+<node TEXT="Overview" ID="ID_1563878077" CREATED="1430766339764" MODIFIED="1471556194271">
+<node TEXT="What is Software Engineering?" ID="ID_428361757" CREATED="1430766356053" MODIFIED="1471556345017">
+<node TEXT="Computer Science is Problem Solving" LOCALIZED_STYLE_REF="styles.topic" ID="ID_512083072" CREATED="1433015560480" MODIFIED="1471556023868"/>
+<node TEXT="Software Engineering is Problem Solving" ID="ID_218608151" CREATED="1471555975338" MODIFIED="1471556067170">
+<node TEXT="On Time" ID="ID_1909221364" CREATED="1471556040132" MODIFIED="1471556043568"/>
+<node TEXT="Within Budget" ID="ID_480350137" CREATED="1471556044307" MODIFIED="1471556051183"/>
+<node TEXT="Quality Assurance" ID="ID_264588606" CREATED="1471556068363" MODIFIED="1471556080609"/>
+</node>
+</node>
+<node TEXT="What do you expect to get out of this course?" ID="ID_284384720" CREATED="1430766388776" MODIFIED="1430766398589"/>
+<node TEXT="What do I expect you to get out of this course?" ID="ID_1332364881" CREATED="1430766399080" MODIFIED="1430766408608">
+<node TEXT="Focus on Principles" LOCALIZED_STYLE_REF="styles.topic" ID="ID_311307422" CREATED="1431978643619" MODIFIED="1471556139535"/>
+<node TEXT="Understand tools used in real world" ID="ID_1304693410" CREATED="1471556104787" MODIFIED="1471556123591"/>
+<node TEXT="Work in effective teams" ID="ID_781767369" CREATED="1471556145219" MODIFIED="1471556172903"/>
+</node>
+<node TEXT="The Two Ways" ID="ID_1784964871" CREATED="1471556212763" MODIFIED="1471556243542">
+<attribute NAME="textbook" VALUE="01-003"/>
+<node TEXT="There are two ways of constructing a software design.  One way is to make it so simple that there are obviously no deficiencies.  The other way is to make it so complicated that there are no obvious deficiencies.  The first method is far more difficult. --C.A.R. Hoare" ID="ID_912659319" CREATED="1471556258099" MODIFIED="1471556334248"/>
+</node>
+</node>
+<node TEXT="Phases" ID="ID_935329290" CREATED="1471556354155" MODIFIED="1471556359909">
+<node TEXT="Requirements Gathering" ID="ID_1705144862" CREATED="1471556359912" MODIFIED="1471556499368">
+<attribute NAME="textbook" VALUE="01-004"/>
+<node TEXT="What are you building?" ID="ID_1846903376" CREATED="1471556449315" MODIFIED="1471556456304"/>
+<node TEXT="Who are you building it for?" ID="ID_1959238700" CREATED="1471556456746" MODIFIED="1471556466999"/>
+<node TEXT="Change Happens" ID="ID_1456256667" CREATED="1471556467643" MODIFIED="1471556483080"/>
+</node>
+<node TEXT="High Level Design" ID="ID_1553570998" CREATED="1471556374603" MODIFIED="1471556529097">
+<attribute NAME="textbook" VALUE="01-005"/>
+<node TEXT="What are the major components" ID="ID_233493695" CREATED="1471556535443" MODIFIED="1471556544847"/>
+<node TEXT="What external systems do you interact with" ID="ID_499267821" CREATED="1471556557546" MODIFIED="1471556569591"/>
+<node TEXT="User interaction" ID="ID_621996646" CREATED="1471556587075" MODIFIED="1471556592111"/>
+</node>
+<node TEXT="Low Level Design" ID="ID_100029663" CREATED="1471556381140" MODIFIED="1471557154851">
+<attribute NAME="textbook" VALUE="01-006"/>
+<node TEXT="What are the implementation level components" ID="ID_1361079626" CREATED="1471556595499" MODIFIED="1471556603559"/>
+<node TEXT="What are there relationships" ID="ID_1791372510" CREATED="1471556603867" MODIFIED="1471556609891"/>
+</node>
+<node TEXT="Development" ID="ID_925784819" CREATED="1471556393043" MODIFIED="1471556395984">
+<node TEXT="Writing Code" ID="ID_1861346698" CREATED="1471556987955" MODIFIED="1471556999823"/>
+</node>
+<node TEXT="Testing" ID="ID_1407396559" CREATED="1471556396370" MODIFIED="1471557171002">
+<attribute NAME="textbook" VALUE="01-007"/>
+<node TEXT="Verification - Did you build the right thing" ID="ID_806962794" CREATED="1471557001708" MODIFIED="1471557010047"/>
+<node TEXT="Validation - Did you build it right" ID="ID_732134267" CREATED="1471557010443" MODIFIED="1471557019167"/>
+</node>
+<node TEXT="Deployment" ID="ID_591773449" CREATED="1471556399211" MODIFIED="1471557192316">
+<attribute NAME="textbook" VALUE="01-008"/>
+<node TEXT="How do you get the code to the customer" ID="ID_1588854122" CREATED="1471557022683" MODIFIED="1471557040783"/>
+</node>
+<node TEXT="Maintenance" ID="ID_617845557" CREATED="1471556410347" MODIFIED="1471557205025">
+<attribute NAME="textbook" VALUE="01-008"/>
+<node TEXT="Only if you succeed" ID="ID_392931243" CREATED="1471557044859" MODIFIED="1471557075127"/>
+<node TEXT="Bug fixes" ID="ID_472620717" CREATED="1471557075475" MODIFIED="1471557078927"/>
+<node TEXT="Updates" ID="ID_1225959747" CREATED="1471557079211" MODIFIED="1471557082287"/>
+</node>
+</node>
+</node>
+<node TEXT="Project" POSITION="right" ID="ID_1858312082" CREATED="1471557260075" MODIFIED="1471557263389">
+<node TEXT="Teams" ID="ID_1865151516" CREATED="1471557263391" MODIFIED="1471557266080">
+<node TEXT="Team Structure" ID="ID_1273971057" CREATED="1471557294139" MODIFIED="1471557312123">
+<node TEXT="Teams will have 3-5 members" ID="ID_1544898071" CREATED="1471557312126" MODIFIED="1471557329576"/>
+<node TEXT="You will need time to coordinate - so make sure schedules match" ID="ID_1335156565" CREATED="1471557331019" MODIFIED="1471557365527"/>
+<node TEXT="Diversity of skills will be a bonus" ID="ID_626707553" CREATED="1471557368091" MODIFIED="1471557380344"/>
+</node>
+<node TEXT="Tools" ID="ID_1239207185" CREATED="1471557382707" MODIFIED="1471557395115">
+<node TEXT="GitHub" ID="ID_865918305" CREATED="1471557395118" MODIFIED="1471557452448">
+<node TEXT="All code will be hosted on GitHub" ID="ID_1919714770" CREATED="1471557453459" MODIFIED="1471557466304"/>
+<node TEXT="Provides issue tracking" ID="ID_1561874615" CREATED="1471557466667" MODIFIED="1471557471648"/>
+<node TEXT="Provides peer review of code" ID="ID_634691959" CREATED="1471557471891" MODIFIED="1471557482136"/>
+</node>
+<node TEXT="CMake" ID="ID_1135549910" CREATED="1471557483203" MODIFIED="1471557485560">
+<node TEXT="Cross platform build system" ID="ID_44816397" CREATED="1471557486507" MODIFIED="1471557539247"/>
+</node>
+<node TEXT="externpro" ID="ID_285475002" CREATED="1471557495707" MODIFIED="1471557498899">
+<node TEXT="Provides 3rd party libraries" ID="ID_119476666" CREATED="1471557498902" MODIFIED="1471557530144"/>
+</node>
+<node TEXT="Compiler" ID="ID_783046370" CREATED="1471557540587" MODIFIED="1471557542963">
+<node TEXT="VS 2015" ID="ID_484462267" CREATED="1471557542966" MODIFIED="1471557546560"/>
+<node TEXT="gcc 5.4" ID="ID_35467755" CREATED="1471557546787" MODIFIED="1471557552336"/>
+<node TEXT="We will use C++ and it will be cross platform" ID="ID_1325567299" CREATED="1471557552771" MODIFIED="1471557562712"/>
+</node>
+</node>
+</node>
+<node TEXT="Requirements (For Now ...)" ID="ID_1890060243" CREATED="1471557266579" MODIFIED="1471557282103"/>
+</node>
+</node>
+</map>
