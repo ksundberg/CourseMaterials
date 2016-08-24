2715b82fa345fc787bdb90293d88ebad57c3db91
Kenneth Sundberg <ksundberg@sdl.usu.edu>
2016-08-24 17:10:48 -0600

    Initial lectures for graphics

---
+++ cs5400/lectures/01-Introduction.mm
@@ -0,0 +1,160 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="01- Introduction" ID="ID_729166855" CREATED="1430766178119" MODIFIED="1472077304279">
+<attribute NAME="author" VALUE="Kenneth Sundberg"/>
+<attribute NAME="date" VALUE=""/>
+<attribute NAME="subtitle" VALUE="CS 5400"/>
+<hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Syllabus" POSITION="right" ID="ID_405778426" CREATED="1430766242153" MODIFIED="1432567614206">
+<node TEXT="Topic Overview" ID="ID_1478149906" CREATED="1430766266105" MODIFIED="1430766274717">
+<node TEXT="Course Description" ID="ID_1245100126" CREATED="1433013226707" MODIFIED="1433013240413">
+<node TEXT="IIntroduction to concepts of graphical techniques. Digital and pictorial representation of information." ID="ID_1608230200" CREATED="1433013260065" MODIFIED="1472076606786"/>
+</node>
+<node TEXT="Textbook" ID="ID_787170715" CREATED="1433014013454" MODIFIED="1433014016745">
+<node TEXT="Interactive Computer Graphics 7th Edition" ID="ID_455122128" CREATED="1472076314110" MODIFIED="1472076385626"/>
+<node TEXT="ISBN: 978-0-13-357484-5" ID="ID_1932376874" CREATED="1433014065333" MODIFIED="1472076377956"/>
+</node>
+</node>
+<node TEXT="Policies" ID="ID_1049242193" CREATED="1430766258561" MODIFIED="1430766265555">
+<node TEXT="Academic Honesty" ID="ID_1414885647" CREATED="1433013754576" MODIFIED="1433013775443">
+<node TEXT="It is your responsibility to pursue your education in an honest way" LOCALIZED_STYLE_REF="styles.topic" ID="ID_1395850290" CREATED="1433013775461" MODIFIED="1433013848269"/>
+<node TEXT="Plagiarism and cheating are not tolerated" ID="ID_1338850376" CREATED="1433013811174" MODIFIED="1433013839327"/>
+<node TEXT="This course involves group work be careful to contribute" ID="ID_805205164" CREATED="1471555619235" MODIFIED="1471555645594"/>
+</node>
+<node TEXT="Legalized Decency" ID="ID_1011156071" CREATED="1433013883972" MODIFIED="1433013909949">
+<node TEXT="ADA" ID="ID_1538023574" CREATED="1433013909960" MODIFIED="1433013923750"/>
+<node TEXT="Sexual Harassment is not okay" ID="ID_46842519" CREATED="1433013924377" MODIFIED="1433013956596"/>
+<node TEXT="Academic Freedom and the Faculty Code" ID="ID_546616331" CREATED="1433013975944" MODIFIED="1433013986979"/>
+</node>
+<node TEXT="Late Policy" ID="ID_306869006" CREATED="1433014313508" MODIFIED="1433014317564">
+<node TEXT="Don&apos;t be late" LOCALIZED_STYLE_REF="styles.topic" ID="ID_892500312" CREATED="1433014317573" MODIFIED="1433014343582"/>
+<node TEXT="Lose 10 points each day" ID="ID_1612600366" CREATED="1433014325572" MODIFIED="1433014361141"/>
+<node TEXT="No work accepted after 3 days" LOCALIZED_STYLE_REF="styles.topic" ID="ID_1644515651" CREATED="1433014361506" MODIFIED="1433014397132"/>
+<node TEXT="No make up work, extra credit or other silliness will be offered" ID="ID_1814432677" CREATED="1433014369265" MODIFIED="1433014389581"/>
+</node>
+</node>
+<node TEXT="Grading" ID="ID_565993697" CREATED="1430766279625" MODIFIED="1430766291435">
+<node TEXT="Peer Reviews" ID="ID_28815729" CREATED="1472076425333" MODIFIED="1472076428921">
+<node TEXT="Peer Reviews of code are a key technique for high quality" ID="ID_1641371795" CREATED="1472076450333" MODIFIED="1472076466935"/>
+<node TEXT="At this level I expect that you can learn from each other effictivly" ID="ID_1945681826" CREATED="1472076467532" MODIFIED="1472076484570"/>
+<node TEXT="After each assignment you will be required to peer review several other student submissions" ID="ID_520650670" CREATED="1472076485117" MODIFIED="1472076516451"/>
+</node>
+<node TEXT="Grade Weights" ID="ID_505058420" CREATED="1433015005586" MODIFIED="1433015035668">
+<node TEXT="Assignments: 60" ID="ID_1423457656" CREATED="1433015035685" MODIFIED="1472076442869"/>
+<node TEXT="Peer Evaluations: 10" ID="ID_715090175" CREATED="1471555690851" MODIFIED="1471555700491"/>
+<node TEXT="Final Exam: 30" ID="ID_1643166868" CREATED="1433015061442" MODIFIED="1471555684711"/>
+</node>
+<node TEXT="Grade Breakdown" ID="ID_980054680" CREATED="1433014753805" MODIFIED="1433014922581">
+<node TEXT="94: A" ID="ID_865688387" CREATED="1433014805743" MODIFIED="1433014863347"/>
+<node TEXT="90: A-" ID="ID_1489290609" CREATED="1433014846009" MODIFIED="1433014859347"/>
+<node TEXT="87: B+" ID="ID_1990024942" CREATED="1433014864432" MODIFIED="1433014874347"/>
+<node TEXT="84: B" ID="ID_1949740908" CREATED="1433014875089" MODIFIED="1433014880276"/>
+<node TEXT="80: B-" ID="ID_348847779" CREATED="1433014880816" MODIFIED="1433014885827"/>
+<node TEXT="77: C+" ID="ID_1841544540" CREATED="1433014886504" MODIFIED="1433014891626"/>
+<node TEXT="74: C" ID="ID_476899600" CREATED="1433014892191" MODIFIED="1433014896091"/>
+<node TEXT="70: C-" ID="ID_188194848" CREATED="1433014896959" MODIFIED="1433014900297"/>
+<node TEXT="Others: F" ID="ID_1629673453" CREATED="1433014901279" MODIFIED="1433014904640"/>
+</node>
+<node TEXT="Curving" ID="ID_704391856" CREATED="1433014923383" MODIFIED="1433014925815">
+<node TEXT="I will never curve down" ID="ID_454219165" CREATED="1433014925827" MODIFIED="1433014937362"/>
+<node TEXT="At my discretion I may curve up at the end of the semester" ID="ID_1977281405" CREATED="1433014937806" MODIFIED="1433014962216"/>
+<node TEXT="Such curves are often small (1 or 2 points at most)" ID="ID_1291513933" CREATED="1433014962717" MODIFIED="1433014990627"/>
+</node>
+<node TEXT="Assignment Grading" ID="ID_1399304959" CREATED="1433015114432" MODIFIED="1433015120592">
+<node TEXT="Assignments have the most weight on purpose" ID="ID_1961528815" CREATED="1433015120600" MODIFIED="1433015135937"/>
+<node TEXT="You can fail every test and quiz and still pass if you do well on the assignments" LOCALIZED_STYLE_REF="styles.topic" ID="ID_969848273" CREATED="1433015136270" MODIFIED="1433015222419"/>
+<node TEXT="Assignments have clear objective grading criteria" ID="ID_850867986" CREATED="1433015193180" MODIFIED="1433015214410"/>
+<node TEXT="I expect most students to get nearly all of the assignment points" ID="ID_933019687" CREATED="1433015156374" MODIFIED="1433015192583"/>
+</node>
+</node>
+</node>
+<node TEXT="Introduction" POSITION="right" ID="ID_91545943" CREATED="1472076668517" MODIFIED="1472076671540">
+<node TEXT="Graphics" ID="ID_1275293385" CREATED="1472076671824" MODIFIED="1472076724069">
+<node TEXT="What is Graphics Programming" ID="ID_1429959424" CREATED="1472076724071" MODIFIED="1472076740597">
+<node TEXT="All aspects of the generation and display of images" ID="ID_1179215955" CREATED="1472076740600" MODIFIED="1472076757377"/>
+<node TEXT="We will focus on real time graphics for this course" ID="ID_754603171" CREATED="1472076778444" MODIFIED="1472076792058"/>
+</node>
+<node TEXT="Applications" ID="ID_59581373" CREATED="1472076812668" MODIFIED="1472076882416">
+<attribute NAME="textbook" VALUE="01-01"/>
+<node TEXT="Information Display" ID="ID_1505465486" CREATED="1472076816015" MODIFIED="1472076825297"/>
+<node TEXT="Simulation" ID="ID_1593521802" CREATED="1472076825612" MODIFIED="1472076828649"/>
+<node TEXT="Design Tools" ID="ID_112800821" CREATED="1472076829060" MODIFIED="1472076832073"/>
+<node TEXT="Improved User Interfaces" ID="ID_707627317" CREATED="1472076832449" MODIFIED="1472076846452"/>
+</node>
+</node>
+<node TEXT="Environment" ID="ID_1220313658" CREATED="1472077017404" MODIFIED="1472077039623">
+<node TEXT="Build System" ID="ID_1286822329" CREATED="1472076922518" MODIFIED="1472076926193">
+<node TEXT="WebGL" ID="ID_302931888" CREATED="1472076927932" MODIFIED="1472076931700">
+<node TEXT="Graphics can be very hardware dependent" ID="ID_489233857" CREATED="1472076931703" MODIFIED="1472076946425"/>
+<node TEXT="WebGL is a portable subset of OpenGL for use in web browsers" ID="ID_302833772" CREATED="1472076946893" MODIFIED="1472076962273"/>
+<node TEXT="Does not have all of the features of the latest OpenGL but adequate for this course" ID="ID_94493867" CREATED="1472076962676" MODIFIED="1472076978078"/>
+<node TEXT="Shaders are written in a C like language" ID="ID_1794566472" CREATED="1472077125620" MODIFIED="1472077132817"/>
+</node>
+<node TEXT="JavaScript" ID="ID_900636050" CREATED="1472076984844" MODIFIED="1472076988373">
+<node TEXT="JavaScript is becoming a de facto standard for web programming" ID="ID_1014960012" CREATED="1472076988375" MODIFIED="1472077002458"/>
+<node TEXT="Interfaces very well with WebGL" ID="ID_1167208572" CREATED="1472077002692" MODIFIED="1472077010537"/>
+<node TEXT="This will be the primary language" ID="ID_1440092843" CREATED="1472077138108" MODIFIED="1472077146209"/>
+</node>
+<node TEXT="HTML" ID="ID_423849768" CREATED="1472077042948" MODIFIED="1472077046608">
+<node TEXT="Wrapper text that will hold the JavaScript and GL shaders." ID="ID_739286274" CREATED="1472077152205" MODIFIED="1472077173300"/>
+<node TEXT="Provides several GUI widgets that will simplify interaction" ID="ID_1286064499" CREATED="1472077173668" MODIFIED="1472077185186"/>
+</node>
+</node>
+<node TEXT="OpenGL" ID="ID_1182870527" CREATED="1472077190012" MODIFIED="1472077194732">
+<node TEXT="OpenGL is widely available.  WebGL is a subset so everything learned will transfer" ID="ID_1701864058" CREATED="1472077194734" MODIFIED="1472077224881"/>
+<node TEXT="If you are interested in using all of the hardware capabilities I would encourage you to play with OpenGL" ID="ID_340500190" CREATED="1472077228252" MODIFIED="1472077250385"/>
+<node TEXT="DirectX is also available on Windows platforms.  The only real differences are in how you spell things all concepts are the same." ID="ID_520669306" CREATED="1472077254860" MODIFIED="1472077289663"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ cs5400/lectures/02-SystemsAndModels.mm
@@ -0,0 +1,84 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="02- Graphics Systems And Models" ID="ID_729166855" CREATED="1430766178119" MODIFIED="1472079961233">
+<attribute NAME="author" VALUE="Kenneth Sundberg"/>
+<attribute NAME="date" VALUE=""/>
+<attribute NAME="subtitle" VALUE="CS 5400"/>
+<hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="System" POSITION="right" ID="ID_1657204363" CREATED="1472080004428" MODIFIED="1472080008979">
+<node TEXT="Components" ID="ID_1892434348" CREATED="1472080008982" MODIFIED="1472080034924">
+<node TEXT="Input" ID="ID_888772761" CREATED="1472080034932" MODIFIED="1472080044571"/>
+<node TEXT="Framebuffer" ID="ID_177371089" CREATED="1472080045011" MODIFIED="1472080047797"/>
+<node TEXT="CPU and GPU" ID="ID_1386053765" CREATED="1472080048231" MODIFIED="1472080053688"/>
+<node TEXT="Output" ID="ID_1463074396" CREATED="1472080054181" MODIFIED="1472080056050"/>
+</node>
+<node TEXT="Images" ID="ID_1691128032" CREATED="1472080066755" MODIFIED="1472080076707">
+<node TEXT="Images" ID="ID_703452497" CREATED="1472080076714" MODIFIED="1472080078826"/>
+<node TEXT="Pin hole" ID="ID_1841777653" CREATED="1472080079211" MODIFIED="1472080084210"/>
+<node TEXT="Human Visual" ID="ID_1841688976" CREATED="1472080084595" MODIFIED="1472080096168"/>
+</node>
+<node TEXT="API" ID="ID_59937282" CREATED="1472080104027" MODIFIED="1472080107211">
+<node TEXT="WebGL and OpenGL" ID="ID_151653538" CREATED="1472080129555" MODIFIED="1472080135924"/>
+</node>
+<node TEXT="Architectures" ID="ID_1044819420" CREATED="1472080107603" MODIFIED="1472080117346"/>
+</node>
+<node TEXT="Models" POSITION="right" ID="ID_1601060883" CREATED="1472080145851" MODIFIED="1472080168576">
+<node TEXT="Primitives" ID="ID_919901244" CREATED="1472080172755" MODIFIED="1472080176008"/>
+<node TEXT="Color" ID="ID_1961512344" CREATED="1472080176371" MODIFIED="1472080179552"/>
+<node TEXT="Views" ID="ID_558221928" CREATED="1472080181203" MODIFIED="1472080192792"/>
+<node TEXT="Controls" ID="ID_103919496" CREATED="1472080193259" MODIFIED="1472080196808"/>
+</node>
+<node TEXT="WebGL" POSITION="right" ID="ID_1513715239" CREATED="1472080169043" MODIFIED="1472080171593">
+<node TEXT="The Gasket" ID="ID_617157380" CREATED="1472080201515" MODIFIED="1472080210608"/>
+</node>
+</node>
+</map>
