06d56b47c009a9ae2f8be99639802d0e3936c7af
Dr. Kenneth Sundberg <kasundberg@gmail.com>
2017-04-04 23:56:03 -0600

    Add Round Robin to initial main + some refactoring

--- cs3100/projects/scheduler/scheduler_main.cpp
+++ cs3100/projects/scheduler/scheduler_main.cpp
@@ -4,31 +4,56 @@
 #include <iostream>
 #include <limits>
 
-int main()
+namespace
 {
-  /*TODO vary the simulation parameters to get richer results for your report*/
-  cs3100::SimulationParameters p;
-  p.cpus = 4;
-  p.devices = 2;
-  p.cacheSize = 0;
-  p.contextSwitchCost = 0.1f;
-  p.cacheMissCost = 1.0f;
-  p.maximumTimeSlice = std::numeric_limits<float>::max();
-  p.jobs = 10;
-  p.meanTimeBetweenJobs = 10.0f;
-  p.stddevTimeBetweenJobs = 2.0f;
-  // create simulation with specific parameters and algorithms
-  cs3100::Simulation s(p,
-                       std::make_unique<cs3100::FifoReadyQueue>(),
-                       std::make_unique<cs3100::AlwaysInCache>());
-  // run simulation
-  s.run();
+  void report(cs3100::Simulation& s)
+  {
   /*TODO create a report based on the results in s*/
-  std::cout << "Efficiency : " << s.getEfficiency() << std::endl;
+    std::cout << "Efficiency : " << s.getEfficiency() << std::endl;
   std::cout << "Task\tLatency\tResponseTime" << std::endl;
   for (size_t i = 0; i < s.getJobs(); ++i)
   {
     std::cout << i << "\t" << s.rawLatency(i) << "\t" << s.rawResponseTime(i)
               << std::endl;
   }
+
+  }
+
+  template<typename ReadyType, typename CacheType>
+  void runSimulation(cs3100::SimulationParameters const &p)
+  {
+    cs3100::Simulation s(p,std::make_unique<ReadyType>(),std::make_unique<CacheType>());
+    s.run();
+    report(s);
+  }
 }
+
+int main()
+{
+  /*TODO vary the simulation parameters to get richer results for your report*/
+  cs3100::SimulationParameters fifo;
+  fifo.cpus = 4;
+  fifo.devices = 2;
+  fifo.cacheSize = 0;
+  fifo.contextSwitchCost = 0.1f;
+  fifo.cacheMissCost = 1.0f;
+  fifo.maximumTimeSlice = std::numeric_limits<float>::max();
+  fifo.jobs = 10;
+  fifo.meanTimeBetweenJobs = 10.0f;
+  fifo.stddevTimeBetweenJobs = 2.0f;
+  // create simulation with specific parameters and algorithms
+    runSimulation<cs3100::FifoReadyQueue,cs3100::AlwaysInCache>(fifo);
+
+   cs3100::SimulationParameters roundRobin;
+  roundRobin.cpus = 4;
+  roundRobin.devices = 2;
+  roundRobin.cacheSize = 0;
+  roundRobin.contextSwitchCost = 0.1f;
+  roundRobin.cacheMissCost = 1.0f;
+  roundRobin.maximumTimeSlice = 5.0f;
+  roundRobin.jobs = 10;
+  roundRobin.meanTimeBetweenJobs = 10.0f;
+  roundRobin.stddevTimeBetweenJobs = 2.0f;
+  // create simulation with specific parameters and algorithms
+    runSimulation<cs3100::FifoReadyQueue,cs3100::AlwaysInCache>(roundRobin);
+  }
