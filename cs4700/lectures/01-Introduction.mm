5a4847d137c5ae3e94e72b0b770a362756e59c61
Kenneth Sundberg <ksundberg@sdl.usu.edu>
2017-06-14 17:04:10 -0600

    Add cs4700 introduction

---
+++ cs4700/lectures/01-Introduction.mm
@@ -0,0 +1,105 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Introduction" ID="ID_192476694" CREATED="1388881465436" MODIFIED="1497481008665"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<attribute NAME="author" VALUE="Kenneth Sundberg"/>
+<attribute NAME="date" VALUE=""/>
+<attribute NAME="subtitle" VALUE="CS 4700"/>
+<node TEXT="Sylabus" POSITION="right" ID="ID_1893758557" CREATED="1388724830529" MODIFIED="1388724842531">
+<node TEXT="Topic" ID="ID_1859463312" CREATED="1388724860429" MODIFIED="1388724875730">
+<node TEXT="Programming Languages" ID="ID_164152150" CREATED="1388724901404" MODIFIED="1497481227985">
+<node TEXT="Language Evaluation" ID="ID_224733240" CREATED="1388881574931" MODIFIED="1497481120534"/>
+<node TEXT="Syntax and Semantics" ID="ID_1684074757" CREATED="1497481155367" MODIFIED="1497481162293"/>
+<node TEXT="Language Features" ID="ID_848708801" CREATED="1497481121208" MODIFIED="1497481124677"/>
+<node TEXT="Functional and Logic Languages" ID="ID_520965854" CREATED="1497481125111" MODIFIED="1497481131861"/>
+</node>
+<node TEXT="Why Study Programming Languages?" ID="ID_1450989463" CREATED="1497481191247" MODIFIED="1497481218833">
+<attribute NAME="textbook" VALUE="01-01"/>
+<node TEXT="Express Ideas" ID="ID_1412444639" CREATED="1497481258744" MODIFIED="1497481289509">
+<node TEXT="Improve vocabulary" ID="ID_199011018" CREATED="1497481372872" MODIFIED="1497481378117"/>
+<node TEXT="Expose to different ideas" ID="ID_1151489960" CREATED="1497481378472" MODIFIED="1497481385877"/>
+</node>
+<node TEXT="Better background" ID="ID_457992538" CREATED="1497481290176" MODIFIED="1497481296016">
+<node TEXT="Learning a new language" ID="ID_1471488040" CREATED="1497481296020" MODIFIED="1497481305981"/>
+<node TEXT="Selecting a language" ID="ID_578594139" CREATED="1497481306391" MODIFIED="1497481312245"/>
+</node>
+<node TEXT="Intuition" ID="ID_169152356" CREATED="1497481313968" MODIFIED="1497481351120">
+<node TEXT="Better understanding of implementation details" ID="ID_1619537090" CREATED="1497481351124" MODIFIED="1497481360189"/>
+<node TEXT="Better use of known languages" ID="ID_1631098209" CREATED="1497481360599" MODIFIED="1497481369373"/>
+</node>
+</node>
+<node TEXT="Textbook" ID="ID_343747052" CREATED="1388724909739" MODIFIED="1388724912876">
+<node TEXT="The text for the course is &quot;Concepts of Programming Languages&quot; by Robert W. Sebesta" ID="ID_1144774546" CREATED="1388724912881" MODIFIED="1497481070839"/>
+<node TEXT="Any version of the book" ID="ID_898662885" CREATED="1388881678527" MODIFIED="1388881696212"/>
+<node TEXT="Slide references from the eleventh edition" ID="ID_821065012" CREATED="1451515859133" MODIFIED="1497481082796"/>
+</node>
+</node>
+<node TEXT="Grading" ID="ID_1678855989" CREATED="1388724876309" MODIFIED="1388724885234">
+<node TEXT="Grade Break Down" ID="ID_230602766" CREATED="1388724969018" MODIFIED="1388724978290">
+<node TEXT="Programming Assignments - 60\%" ID="ID_1814846665" CREATED="1388724978294" MODIFIED="1388733545146"/>
+<node TEXT="Exams - 30\%" ID="ID_337805349" CREATED="1388724988985" MODIFIED="1388733548347"/>
+<node TEXT="Quizzes - 10\%" ID="ID_720841499" CREATED="1388724994297" MODIFIED="1388881855270"/>
+</node>
+<node TEXT="Late Policy" ID="ID_1889739863" CREATED="1388725028975" MODIFIED="1388725041760">
+<node TEXT="10 \% per day late" ID="ID_299689484" CREATED="1388725041764" MODIFIED="1388733682881"/>
+<node TEXT="No work accepted after 3 days" ID="ID_624640154" CREATED="1388725048623" MODIFIED="1388725055068"/>
+<node TEXT="No exceptions - even if life happens" ID="ID_1219399267" CREATED="1388725055383" MODIFIED="1388725069035"/>
+</node>
+</node>
+<node TEXT="Policies" ID="ID_805744572" CREATED="1388724886277" MODIFIED="1388724899857">
+<node TEXT="ADA" ID="ID_1235370219" CREATED="1388725072150" MODIFIED="1388725075734">
+<node TEXT="This course is ADA compliant as required" ID="ID_575281479" CREATED="1388725075739" MODIFIED="1388725084907"/>
+<node TEXT="Discuss with the DRC for arrangments" ID="ID_963702389" CREATED="1388725085750" MODIFIED="1388725094155"/>
+</node>
+</node>
+</node>
+</node>
+</map>
