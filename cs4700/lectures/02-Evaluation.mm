b7cc57d583f099a6488d23991c72325f6543ca90
Dr. Kenneth Sundberg <kasundberg@gmail.com>
2017-06-23 11:37:19 -0600

    Partial work on evaluation lecture

---
+++ cs4700/lectures/02-Evaluation.mm
@@ -0,0 +1,86 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Evaluation" ID="ID_192476694" CREATED="1388881465436" MODIFIED="1498239000486"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<attribute NAME="author" VALUE="Kenneth Sundberg"/>
+<attribute NAME="date" VALUE=""/>
+<attribute NAME="subtitle" VALUE="CS 4700"/>
+<node TEXT="Domains" POSITION="right" ID="ID_740067642" CREATED="1498239005214" MODIFIED="1498239017996">
+<node TEXT="Scientific" ID="ID_1564937143" CREATED="1498239018001" MODIFIED="1498239077278">
+<attribute NAME="textbook" VALUE="01-02"/>
+</node>
+<node TEXT="Business" ID="ID_1062055328" CREATED="1498239021765" MODIFIED="1498239024782"/>
+<node TEXT="Artificial Intelligence" ID="ID_1202516038" CREATED="1498239025265" MODIFIED="1498239047683"/>
+<node TEXT="Web Software" ID="ID_304362294" CREATED="1498239052885" MODIFIED="1498239057460"/>
+</node>
+<node TEXT="Language Criteria" POSITION="right" ID="ID_413567925" CREATED="1498239081679" MODIFIED="1498239100788">
+<node TEXT="Readability" ID="ID_1859202167" CREATED="1498239102180" MODIFIED="1498239122888">
+<attribute NAME="textbook" VALUE="01-03"/>
+</node>
+<node TEXT="Writability" ID="ID_1819305565" CREATED="1498239129203" MODIFIED="1498239153161"/>
+<node TEXT="Reliability" ID="ID_617808288" CREATED="1498239153634" MODIFIED="1498239158952"/>
+</node>
+<node TEXT="Language Characteristics" POSITION="right" ID="ID_755836842" CREATED="1498239166689" MODIFIED="1498239174758">
+<node TEXT="Simplicity" ID="ID_1539028629" CREATED="1498239175992" MODIFIED="1498239196939"/>
+<node TEXT="Orthogonality" ID="ID_691512560" CREATED="1498239197413" MODIFIED="1498239202643"/>
+<node TEXT="Data Types" ID="ID_1198812784" CREATED="1498239202962" MODIFIED="1498239206226"/>
+<node TEXT="Syntax Design" ID="ID_1732075740" CREATED="1498239206575" MODIFIED="1498239210938"/>
+<node TEXT="Abstraction Support" ID="ID_1060627195" CREATED="1498239211333" MODIFIED="1498239219375"/>
+<node TEXT="Expressivity" ID="ID_298135153" CREATED="1498239219751" MODIFIED="1498239229541"/>
+<node TEXT="Type Checking" ID="ID_1585702305" CREATED="1498239233854" MODIFIED="1498239238851"/>
+<node TEXT="Exception Handling" ID="ID_980850627" CREATED="1498239239185" MODIFIED="1498239246063"/>
+<node TEXT="Restricted Aliasing" ID="ID_1242485958" CREATED="1498239246501" MODIFIED="1498239254047"/>
+</node>
+<node TEXT="Programmer Priorities" POSITION="right" ID="ID_176979757" CREATED="1498239259796" MODIFIED="1498239271800">
+<node TEXT="Correctness" ID="ID_243715076" CREATED="1498239271804" MODIFIED="1498239275633"/>
+</node>
+</node>
+</map>
