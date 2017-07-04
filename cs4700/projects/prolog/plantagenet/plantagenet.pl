19c8087bbcce23873765c7bf84491fdab0c68d7b
Dr. Kenneth Sundberg <kasundberg@gmail.com>
2017-07-04 11:43:51 -0600

    Add normans to plantagenet.pl

---
+++ cs4700/projects/prolog/plantagenet/plantagenet.pl
@@ -0,0 +1,97 @@
+male('William the Conqueror').
+king('William the Conqueror').
+
+female('Matilda of Flanders').
+
+male('Robert Curthose').
+parent('William the Conqueror','Robert Curthose').
+parent('Matilda of Flanders','Robert Curthose').
+
+male('Richard duke of Bernay').
+parent('William the Conqueror','Richard duke of Bernay').
+parent('Matilda of Flanders','Richard duke of Bernay').
+
+female('Adeliza').
+parent('William the Conqueror','Adeliza').
+parent('Matilda of Flanders','Adeliza').
+
+female('Cecily').
+parent('William the Conqueror','Cecily').
+parent('Matilda of Flanders','Cecily').
+
+female('Agatha').
+parent('William the Conqueror','Agatha').
+parent('Matilda of Flanders','Agatha').
+
+female('Adela').
+parent('William the Conqueror','Adela').
+parent('Matilda of Flanders','Adela').
+
+male('Henry I').
+king('Henry I').
+parent('William the Conqueror','Henry I').
+parent('Matilda of Flanders','Henry I').
+successor('William II','Henry I').
+
+female('Edith of Scotland').
+
+male('Stephen count of Blois').
+
+male('William II').
+king('William II').
+parent('William the Conqueror','William II').
+parent('Matilda of Flanders','William II').
+successor('William the Conqueror','William II').
+
+male('Henry of Blois').
+parent('Adela','Henry of Blois').
+parent('Stephen count of Blois','Henry of Blois').
+
+male('Theobald count of Blois').
+parent('Adela','Theobald count of Blois').
+parent('Stephen count of Blois','Theobald count of Blois').
+
+male('King Stephen').
+king('King Stephen').
+successor('Henry I','King Stephen').
+parent('Adela','King Stephen').
+parent('Stephen count of Blois','King Stephen').
+
+female('Matilda of Boulogne').
+
+male('Eustace count of Boulogne').
+parent('Matilda of Boulogne','Eustace count of Boulogne').
+parent('King Stephen','Eustace count of Boulogne').
+
+male('William earl of Surrey').
+parent('Matilda of Boulogne','William earl of Surrey').
+parent('King Stephen','William earl of Surrey').
+
+male('William the Aetheling').
+parent('Henry I','William the Aetheling').
+parent('Edith of Scotland','William the Aetheling').
+
+female('Empress Matilda').
+parent('Henry I','Empress Matilda').
+parent('Edith of Scotland','Empress Matilda').
+
+male('Geoffrey Plantagenet').
+
+male('Henry II').
+king('Henry II').
+successor('King Stephen','Henry II').
+parent('Geoffrey Plantagenet','Henry II').
+parent('Empress Matilda','Henry II').
+
+male('Geoffrey count of Nantes').
+parent('Geoffrey Plantagenet','Geoffrey count of Nantes').
+parent('Empress Matilda','Geoffrey count of Nantes').
+
+male('William count of Poitou').
+parent('Geoffrey Plantagenet','William count of Poitou').
+parent('Empress Matilda','William count of Poitou').
+
+female('Eleanor of Aquitaine').
+
+
+
