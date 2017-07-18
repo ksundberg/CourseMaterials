523530edf4f577707bc87ff97e5407478071fbf2
Kenneth Sundberg <ksundberg@sdl.usu.edu>
2017-07-18 16:32:03 -0600

    More outlining

---
+++ cs4700/lectures/03-SyntaxSemantics.mm
@@ -0,0 +1,100 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Syntax and Semantics" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1500350691443"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6"/>
+<attribute NAME="author" VALUE="Kenneth Sundberg"/>
+<attribute NAME="subtitle" VALUE="CS 4700"/>
+<node TEXT="Syntax" POSITION="right" ID="ID_1402361844" CREATED="1500349473387" MODIFIED="1500349796258">
+<edge COLOR="#ff0000"/>
+<node TEXT="Lexemes" ID="ID_462591628" CREATED="1500349625955" MODIFIED="1500349792698">
+<attribute NAME="textbook" VALUE="03-02"/>
+</node>
+<node TEXT="Recognizers" ID="ID_1718392118" CREATED="1500349767891" MODIFIED="1500349770778"/>
+<node TEXT="Generators" ID="ID_872672935" CREATED="1500349771445" MODIFIED="1500349773151"/>
+<node TEXT="Context Free Grammars" ID="ID_389745230" CREATED="1500349802395" MODIFIED="1500349822321">
+<attribute NAME="textbook" VALUE="03-03"/>
+</node>
+<node TEXT="Ambiguity" ID="ID_483661089" CREATED="1500349842179" MODIFIED="1500349845112"/>
+<node TEXT="Operator precedence" ID="ID_601840039" CREATED="1500349961483" MODIFIED="1500349965944"/>
+</node>
+<node TEXT="Semantics" POSITION="right" ID="ID_1369099253" CREATED="1500349978427" MODIFIED="1500349986440">
+<edge COLOR="#00ff00"/>
+<node TEXT="Static Semantics" ID="ID_1538235156" CREATED="1500349986931" MODIFIED="1500350083969">
+<attribute NAME="textbook" VALUE="03-04"/>
+</node>
+<node TEXT="Attribute Grammars" ID="ID_657797324" CREATED="1500350086579" MODIFIED="1500350092024"/>
+<node TEXT="Arithmatic Grammar" ID="ID_61499148" CREATED="1500350092419" MODIFIED="1500350236353"/>
+<node TEXT="Dynamic Semantics" ID="ID_588101664" CREATED="1500350244396" MODIFIED="1500350257659">
+<attribute NAME="textbook" VALUE="03-05"/>
+</node>
+<node TEXT="Operational Semantics" ID="ID_1411333744" CREATED="1500350303148" MODIFIED="1500350309673"/>
+<node TEXT="Denotaional Semantics" ID="ID_1407608982" CREATED="1500350310156" MODIFIED="1500350331057"/>
+<node TEXT="Axiomatic Semantics" ID="ID_1390733064" CREATED="1500350367940" MODIFIED="1500350373329"/>
+</node>
+<node TEXT="Lexical Analysis" POSITION="right" ID="ID_894477464" CREATED="1500350438068" MODIFIED="1500350455500">
+<edge COLOR="#ff00ff"/>
+<attribute NAME="textbook" VALUE="04-02"/>
+<node TEXT="Finite Automata" ID="ID_1266848958" CREATED="1500350472156" MODIFIED="1500350481145"/>
+<node TEXT="Regular Expressions" ID="ID_1133713139" CREATED="1500350481483" MODIFIED="1500350484512"/>
+</node>
+<node TEXT="Parsing" POSITION="right" ID="ID_991931485" CREATED="1500350492852" MODIFIED="1500350503235">
+<edge COLOR="#ffff00"/>
+<attribute NAME="textbook" VALUE="04-03"/>
+<node TEXT="Recursive Descent" ID="ID_980913813" CREATED="1500350504164" MODIFIED="1500350599475">
+<attribute NAME="textbook" VALUE="04-04"/>
+</node>
+<node TEXT="LR Parser" ID="ID_314210539" CREATED="1500350519772" MODIFIED="1500350632908">
+<attribute NAME="textbook" VALUE="04-05"/>
+</node>
+</node>
+</node>
+</map>
---
+++ cs4700/lectures/04-Names.mm
@@ -0,0 +1,67 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Names and Bindings" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1500350842707"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<node TEXT="Identifiers" POSITION="right" ID="ID_1957596854" CREATED="1500396522669" MODIFIED="1500396541612">
+<edge COLOR="#ff0000"/>
+<attribute NAME="textbook" VALUE="05-02"/>
+<node TEXT="Name forms" ID="ID_1066381124" CREATED="1500396556253" MODIFIED="1500396564574">
+<node TEXT="What are legal characters" ID="ID_1774882158" CREATED="1500396564576" MODIFIED="1500396574675"/>
+<node TEXT="Are names case sensitive" ID="ID_1915480575" CREATED="1500396575125" MODIFIED="1500396582674"/>
+</node>
+<node TEXT="Special words" ID="ID_443212069" CREATED="1500396584629" MODIFIED="1500396588394">
+<node TEXT="Key words" ID="ID_253970838" CREATED="1500396588397" MODIFIED="1500396590842"/>
+<node TEXT="Reserved Words" ID="ID_203052421" CREATED="1500396591238" MODIFIED="1500396594609"/>
+</node>
+</node>
+</node>
+</map>
