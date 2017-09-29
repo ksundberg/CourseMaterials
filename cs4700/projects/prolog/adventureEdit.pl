bba669d4786e9ca93752e2c476d7039b5a4ed251
Evan Boren <egelion47@gmail.com>
2017-09-29 09:03:02 -0600

    Created adventureEdit.pl
    Evan Boren A00582356. Added some now long descriptions.

---
+++ cs4700/projects/prolog/adventureEdit.pl
@@ -0,0 +1,273 @@
+:- dynamic here/1.
+:- dynamic has/1.
+:- dynamic location/2.
+here(bedroom).
+
+room(animal_science).
+room(avenue).
+room(bedroom).
+room(chemistry_lab).
+room(common_room).
+room(computer_lab).
+room(eslc_north).
+room(eslc_south).
+room(elevator).
+room(geology_building).
+room(green_beam).
+room(hall).
+room(hub).
+room(kitchen).
+room(laser_lab).
+room(library).
+room(observatory).
+room(old_main).
+room(plaza).
+room(quad).
+room(roof).
+room(roommate_room).
+room(ser_1st_floor).
+room(ser_2nd_floor).
+room(secret_lab).
+room(special_collections).
+room(tsc_patio).
+room(tunnels_east).
+room(tunnels_north).
+room(tunnels_west).
+
+door(eslc_north,chemistry_lab).
+door(eslc_north,tsc_patio).
+door(eslc_south,eslc_north).
+door(hub,tunnels_west).
+door(old_main,computer_lab).
+door(ser_1st_floor,elevator).
+door(ser_2nd_floor,laser_lab).
+door(ser_2nd_floor,elevator).
+door(tsc_patio,hub).
+door(tunnels_east,ser_1st_floor).
+door(tunnels_north,animal_science).
+door(tunnels_north,tunnels_west).
+door(tunnels_west,secret_lab).
+door(tunnels_west,tunnels_east).
+door(avenue,quad).
+door(bedroom,hall).
+door(hall,common_room).
+door(hall,roommate_room).
+door(kitchen,hall).
+door(library,special_collections).
+door(library,plaza).
+door(plaza,common_room).
+door(plaza,ser_1st_floor).
+door(plaza,avenue).
+door(quad,animal_science).
+door(quad,eslc_south).
+door(quad,geology_building).
+door(quad,old_main).
+door(quad,tsc_patio).
+door(roof,green_beam).
+door(roof,elevator).
+door(roof,observatory).
+
+location(closet,eslc_south).
+location(goggles,closet).
+location(note,bedroom).
+location(book_a,special_collections).
+location(book_b,special_collections).
+location(book_c,special_collections).
+location(recipe,book_a).
+location(bone,geology_building).
+location(fly,roommate_room).
+location(key,coat).
+location(coat,green_beam).
+location(large_disk,pylon_a).
+location(medium_disk,pylon_a).
+location(small_disk,pylon_a).
+location(pylon_a,secret_lab).
+location(pylon_b,secret_lab).
+location(pylon_c,secret_lab).
+location(flask,chemistry_lab).
+location(bunsen_burner,chemistry_lab).
+location(laser,laser_lab).
+
+container(closet).
+container(coat).
+container(book_a).
+container(pylon_a).
+container(pylon_b).
+container(pylon_c).
+
+equipment(laser).
+equipment(bunsen_burner).
+
+create_recipe(laser,[bone],charged_bone).
+create_recipe(bunsen_burner,[flask,charged_bone,fly],potion).
+
+name(animal_science,"Animal Science Building").
+name(avenue,"A tree lined avenue").
+name(bedroom,"Your bedroom").
+name(bone,"large dinosaur bone").
+name(charged_bone,"a chunk of dragon bone").
+name(book_a,"Corpus Hermiticum").
+name(book_b,"War and Peace").
+name(book_c,"Great Expectations").
+name(bunsen_burner,"bunsen burner").
+name(chemistry_lab,"Student Chemistry Lab").
+name(closet,"equipment closet").
+name(coat,"Dr. Sundberg's lab coat").
+name(common_room,"Dorm common room").
+name(computer_lab,"Student Computer Lab").
+name(elevator,"Elevator").
+name(potion,"potion").
+name(eslc_north,"Eccels Science Learning Center").
+name(eslc_south,"Eccels Science Learning Center").
+name(flask,"glass flask").
+name(fly,"dead fly").
+name(geology_building,"Geology Building").
+name(goggles,"dark saftey goggles").
+name(green_beam,"The 'Grean Beam' enclosure").
+name(hall,"Hallway").
+name(hub,"The Hub").
+name(key,"key").
+name(kitchen,"Kitchen").
+name(large_disk,"large energy disk").
+name(laser,"laser array").
+name(laser_lab,"Laser Lab").
+name(library,"Merill-Caizer Library").
+name(medium_disk,"medium energy disk").
+name(note,"note").
+name(observatory,"Observatory").
+name(old_main,"Old Main").
+name(plaza,"Engineering plaza").
+name(pylon_a,"red pylon").
+name(pylon_b,"blue pylon").
+name(pylon_c,"green pylon").
+name(quad,"The Quad").
+name(recipe,"alchemical recipe").
+name(roof,"On the roof of the SER Building").
+name(roommate_room,"Your dormmate's room").
+name(secret_lab,"Dr. Sundberg's Secret Lab").
+name(ser_1st_floor,"1st Floor of SER Building").
+name(ser_2nd_floor,"2nd Floor of SER Building").
+name(small_disk,"small energy disk").
+name(special_collections,"Special Collections Room").
+name(tsc_patio,"Patio of the TSC").
+name(tunnels_east,"Underground Tunnels").
+name(tunnels_north,"Underground Tunnels").
+name(tunnels_west,"Underground Tunnels").
+name(_,"").
+
+short_desc(animal_science,"").
+short_desc(avenue,"").
+short_desc(bedroom,"").
+short_desc(potion,"An oily black potion").
+short_desc(bone,"a large dark stone").
+short_desc(charged_bone,"A small piece of 'dragon' bone").
+short_desc(book_a,"a copy of 'Corpus Hermiticum' a book on alchemy").
+short_desc(book_b,"a copy of 'War and Peace'").
+short_desc(book_c,"a copy of 'Great Expectations'").
+short_desc(bunsen_burner,"A stand with a tube connected to a natural gas supply").
+short_desc(chemistry_lab,"").
+short_desc(closet,"").
+short_desc(coat,"A large white lab coat with lots of pockets").
+short_desc(common_room,"").
+short_desc(computer_lab,"").
+short_desc(elevator,"You are in a plain metal box.  There are buttons labeled with various locations.").
+short_desc(eslc_north,"You are on the north side of the ESLC.").
+short_desc(eslc_south,"You are on the south side of the ESLC.").
+short_desc(flask,"a glass flask suitable for mixing reagents").
+short_desc(fly,"the partialy squished body of a dead house fly").
+short_desc(geology_building,"").
+short_desc(goggles,"dark eye protection left over from the 'Great American Eclipse'").
+short_desc(green_beam,"Dr. Sundberg is standing at a large machine which is emitting a bright beam of green light.  You overhear a conversation indicating that he has set up a wormhole generator in his secret lab.  This will allow the alien invasion force to reach earth.").
+short_desc(hall,"").
+short_desc(hub,"").
+short_desc(key,"an ornate key glowing with alien energies").
+short_desc(kitchen,"").
+short_desc(large_disk,"a large disk glowing with alien energy").
+short_desc(laser,"").
+short_desc(laser_lab,"").
+short_desc(library,"").
+short_desc(medium_disk,"a medium sized disk glowing with alien energy").
+short_desc(note,"a handwritten note from your roommate").
+short_desc(observatory,"").
+short_desc(old_main,"").
+short_desc(plaza,"A large open space between engineering buildings").
+short_desc(pylon_a,"a glowing red pyramid shaped structure").
+short_desc(pylon_b,"a glowing blue pyramid shaped structure").
+short_desc(pylon_c,"a glowing green pyramid shaped structure").
+short_desc(quad,"").
+short_desc(recipe,"a page from 'Corpus Hermiticum' containing a recipe for an invisibility potion").
+short_desc(roof,"").
+short_desc(roommate_room,"This is your roommate's bedroom. It's a mess, as usual.").
+short_desc(secret_lab,"Really? A secret lab? How quaint.").
+short_desc(ser_1st_floor,"").
+short_desc(ser_2nd_floor,"").
+short_desc(small_disk,"a small disk glowing with alien energy").
+short_desc(special_collections,"").
+short_desc(tsc_patio,"This is the patio outsice the student center, a nice place for a smoke break.").
+short_desc(tunnels_east,"The underground tunnels are a confusing place.  I hope you don't get lost.").
+short_desc(tunnels_north,"The underground tunnels are a confusing place.  I hope you don't get lost.").
+short_desc(tunnels_west,"The underground tunnels are a confusing place.  I hope you don't get lost.").
+short_desc(_,"").
+
+long_desc(animal_science,"This is the animal science building. What kind of math do they do here?").
+long_desc(avenue,"").
+long_desc(bedroom,"").
+long_desc(bone,"").
+long_desc(book_a,"An ancient work on alchemy containing many magical formulae.").
+long_desc(book_b,"The Picture of Dorian Gray.").
+long_desc(book_c,"An old leatherbound book written in a very old language you can't read.").
+long_desc(bunsen_burner,"A small burner used to heat liquid in a flask").
+long_desc(chemistry_lab,"The chemistry lab. Many fun times are to be had here.").
+long_desc(closet,"This is the closet, don't stay in too long").
+long_desc(coat,"It's a coat").
+long_desc(common_room,"").
+long_desc(computer_lab,"The computer lab houses many computers. You need an A number to log in.").
+long_desc(elevator,"Elevator go up. Elevator go down.").
+long_desc(eslc_north,"").
+long_desc(eslc_south,"").
+long_desc(flask,"A flask made of borosilicate glass, it will not crack when heated.").
+long_desc(fly,"eww").
+long_desc(geology_building,"A place of rocks, and the study of them.").
+long_desc(goggles,"Goggles are used to protect your eyes when working with lab equipment.").
+long_desc(green_beam,"Who knows, but I wouldn't cross it.").
+long_desc(hall,"A hallway").
+long_desc(hub,"").
+long_desc(key,"").
+long_desc(kitchen,"").
+long_desc(large_disk,"").
+long_desc(laser,"").
+long_desc(laser_lab,"").
+long_desc(library,"").
+long_desc(medium_disk,"").
+long_desc(note,"In the handwriting of your room mate is hastily scrawled: 'You've got to help.  Dr. Sundberg is an alien and wants to take over the world.  I think he is on to me, you are the only hope left! Try to find out what he is doing with the green beam.'").
+long_desc(observatory,"").
+long_desc(old_main,"").
+long_desc(plaza,"").
+long_desc(pylon_a,"").
+long_desc(pylon_b,"").
+long_desc(pylon_c,"").
+long_desc(quad,"").
+long_desc(recipe,"A potion for invisibility: Bathe the bone of a dragon in pure light.  Distill charged bone with the wings of a fly.  Let cool before quaffing.").
+long_desc(roof,"").
+long_desc(roommate_room,"").
+long_desc(secret_lab,"").
+long_desc(ser_1st_floor,"").
+long_desc(ser_2nd_floor,"").
+long_desc(small_disk,"").
+long_desc(special_collections,"").
+long_desc(tsc_patio,"").
+long_desc(tunnels_east,"").
+long_desc(tunnels_north,"").
+long_desc(tunnels_west,"").
+long_desc(_,"").
+
+puzzle(laser_lab):-has(goggles),!.
+puzzle(laser_lab):-write("It is too dangerous to go in without eye protection."),nl,!,fail.
+puzzle(secret_lab):-has(key),!.
+puzzle(secret_lab):-write("The door is locked, so you can't get in"),nl,!,fail.
+puzzle(green_beam):-has(potion),write("You quaff some potion so that Dr. Sundberg can't see you."),nl.
+puzzle(green_beam):-write("Dr. Sundberg escorts you out saying 'Sorry, this is a restricted area'."),nl,!,fail.
+puzzle(_).
+
+read_words(W):-read_string(user_input,"\n\r","\n\r",_,L),split_string(L,"\t ","\t ",W).
+
